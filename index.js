279f4bd179b84f0cc7c1304ce10a4121c16dab7f
Michael Shick <m@shick.us>
2015-12-03 18:24:24 -0500

    all tools in one project

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-hexo.extend.tag.register("imgix", require("./lib/tag")(hexo));
-hexo.extend.helper.register("imgix", require("./lib/helper")(hexo));
+const config = hexo.config.imgix;
+
+if (config.tag) {
+  hexo.extend.tag.register("imgix", require("./lib/tag")(hexo));
+}
+
+if (config.helper) {
+  hexo.extend.helper.register("imgix", require("./lib/helper")(hexo));
+}
+
+if (config.filter) {
+  hexo.extend.filter.register("before_post_render", require("./lib/filter")(hexo));
+}
+
